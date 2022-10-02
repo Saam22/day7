1700c304c14c65bf0bc331a5afc9bc46659bf962
Saam22 <88535528+Saam22@users.noreply.github.com>
2022-10-02 14:36:32 -0700

    Add files via upload

---
+++ src/Pages/Jsprojects.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Jsprojects.css";
+import ProjectData from "../Data/Jsprojects.json";
+import githubImg from "../assets/img/github.png";
+import pf1 from "../assets/img/project1.png";
+import pf2 from "../assets/img/project2.png";
+import pf3 from "../assets/img/project3.png";
+
+const Jsprojects = () => {
+  return (
+    <>
+      <p>
+        Gym Management System:
+        Description: Design and Develop with object oriented programming
+        Tools:
+        1.Java
+        2.GUI
+        News (WEBSITE)
+        Description: Design Website with all the news, whether political, sports,
+        social
+        Tools:
+        1.Html
+        2.Css
+        3.Javascript
+        Student Report Card System
+        Description: Design System Using Basic of C++
+        Tools:
+        1.C++
+        E-commerce Online
+        Description: Design System Using Data Structure of C++ Tools:
+        1.C++
+        Auction Management System
+        Description: Design System Using Basic of C++
+        Tools:
+        1.C++
+        Http Server
+        Description: Network Project
+        Tools:
+        1.HTTP PROTOCOL
+        2.Socket Programming
+        Film
+        Description: Mobile Application
+        Tools:
+        1.Java
+      </p>
+    </>
+  );
+};
+
+export default Jsprojects;
---
+++ src/Pages/Projects.js
@@ -0,0 +1,54 @@
+import React from "react";
+import onelife from "../Data/Portfolioimg/1life.png";
+import alembicaustralia from "../Data/Portfolioimg/alembicaustralia.png";
+import avidorganics from "../Data/Portfolioimg/avidorganics.png";
+import baggsinc from "../Data/Portfolioimg/baggsinc.png";
+import designmaxinteriors from "../Data/Portfolioimg/designmaxinteriors.png";
+import drdharabhatt from "../Data/Portfolioimg/drdharabhatt.png";
+import mastercoin from "../Data/Portfolioimg/mastercoin.png";
+import thecafemasala from "../Data/Portfolioimg/thecafemasala.png";
+import wimetlab from "../Data/Portfolioimg/wimetlab.png";
+import "./Projects.css";
+
+const Portfolio = () => {
+  return (
+    <>
+      <p>
+        Gym Management System:
+        Description: Design and Develop with object oriented programming
+        Tools:
+        1.Java
+        2.GUI
+        News (WEBSITE)
+        Description: Design Website with all the news, whether political, sports,
+        social
+        Tools:
+        1.Html
+        2.Css
+        3.Javascript
+        Student Report Card System
+        Description: Design System Using Basic of C++
+        Tools:
+        1.C++
+        E-commerce Online
+        Description: Design System Using Data Structure of C++ Tools:
+        1.C++
+        Auction Management System
+        Description: Design System Using Basic of C++
+        Tools:
+        1.C++
+        Http Server
+        Description: Network Project
+        Tools:
+        1.HTTP PROTOCOL
+        2.Socket Programming
+        Film
+        Description: Mobile Application
+        Tools:
+        1.Java
+      </p>
+    </>
+  );
+};
+
+export default Portfolio;
